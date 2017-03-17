6e0b20a0229f152128eb0ce2755a72a960139948
Marc Benedí <marcbenedi.sanmillan@gmail.com>
2017-03-17 15:54:22 +0100

    Tot funcionant

--- CodiPractica/src/Main.java
+++ CodiPractica/src/Main.java
@@ -1,9 +1,18 @@
+import IA.ProbIA5Board;
+
 /**
  * Created by marcbenedi on 17/03/17.
  */
 public class Main {
 
+
+    static int [] prob = new int []{1 ,0, 1, 1, 0};
+    static int [] sol = new int[]{1, 1, 0, 1, 0};
+
+
     public static void main(String[] args){
         System.out.println("Hola");
+        ProbIA5Board myBoard = new ProbIA5Board(prob, sol);
+        myBoard.heuristic();
     }
 }
