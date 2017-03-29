dd76295896b95ad278be16a2162598fa6c1136d0
Marc Benedí <marcbenedi.sanmillan@gmail.com>
2017-03-29 14:20:21 +0200

    Jar created and virtualenv for the script

---
+++ script_experiment_1.py
@@ -0,0 +1,21 @@
+#imports
+import subprocess
+
+#Configuration of the experiment
+seed_c = 1234;
+seed_s = 4321;
+
+num_c = 4;
+num_s = 100;
+
+operant_id = 1;
+initial_solution_id = 1;
+
+#Init of script
+print("Launching experiment 1 with the next configuration:")
+print("seed_c =", seed_c);
+print("seed_s =", seed_s);
+print("num_c =", num_c);
+print("num_s =", num_s);
+print("operant_id =", operant_id);
+print("initial_solution_id =", initial_solution_id);
